d25552fa6a4d1d72288a9e97fbcf9a2f96f3dd49
chutiphon-k <chutiphon.k@ku.th>
2018-05-03 14:07:48 +0700

    Parse date to timestamp

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ export default new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
   parseValue(value) {
-    return new Date(value);
+    return (new Date(value)).getTime();
   },
   parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10));
+    if (ast.kind === _language.Kind.INT) {
+      return (new Date(parseInt(ast.value, 10))).getTime();
     }
 
     return null;
